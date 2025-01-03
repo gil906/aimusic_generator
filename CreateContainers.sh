4321f678c35cfabcf65a99b93628979c2cfbcdb7
Gilbert De Leon <gdleonq@gmail.com>
2025-01-03 14:33:11 -0800

    adding docker compose

--- CreateContainers.sh
+++ CreateContainers.sh
@@ -1,6 +1,6 @@
 # To create teh containers
 docker build -t music-generator /Music_Generator/
-docker build -t /YouTube_Uploader/
+docker build -t youtube-uploader /YouTube_Uploader/
 docker build -t suno-api /Suno_API/
 
 
